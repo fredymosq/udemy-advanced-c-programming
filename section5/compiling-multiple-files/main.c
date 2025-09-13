4c58ddc0079e9f8437a28859df56ccecce8f7762
Fredy Mosquera <25505457+fmosquera0101@users.noreply.github.com>
2025-09-13 06:47:47 -0500

    playing compilling multiple files

---
+++ section5/compiling-multiple-files/main.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+#include "other.h"
+
+int main(int argc, char const *argv[])
+{
+    printf("%d favorite number ", getFavoriteNumber());
+    
+    return 0;
+}
---
+++ section5/compiling-multiple-files/other.c
@@ -0,0 +1,6 @@
+#include "other.h"
+
+int getFavoriteNumber(){
+
+    return 4;
+}
