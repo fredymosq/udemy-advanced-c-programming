9390ae7859ba7b8b32656ef93badcf385e8f4c84
Fredy Mosquera <25505457+fmosquera0101@users.noreply.github.com>
2025-09-14 07:09:56 -0500

    playin with Extern, Auto and Static variables

---
+++ section5/StaticVariable.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+
+int callStaticVariable();
+
+int main(int argc, char const *argv[])
+{
+    callStaticVariable();
+    callStaticVariable();
+    callStaticVariable();
+
+    return 0;
+}
+
+int callStaticVariable(){
+
+    static int count = 0;
+    int myVariable = 0;
+
+    count++;
+    myVariable++;
+
+    printf("\nthe function callStaticVariable has been executed: %i times, \nand the value of myVariable is: %i", count, myVariable);
+
+
+    return count;
+}
