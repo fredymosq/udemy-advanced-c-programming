c3aa9cbf547a0e337d1659bccbe1b10907cd36ad
Fredy Mosquera <25505457+fmosquera0101@users.noreply.github.com>
2025-09-21 07:40:04 -0500

    challenge using recursion

--- section11/RecursionChallenge.c
+++ section11/RecursionChallenge.c
@@ -4,12 +4,18 @@
 #include <stdio.h>
 
 int addNumbers(int n);
+int findGCD(int number1, int number2);
 
 int main(int argc, char const *argv[])
 {
     int n = 5;
     int sum = addNumbers(n);
     printf("The sum from 1 to %i = %i", n, sum);
+
+    int number1 = 10;
+    int number2 = 50;
+    int gcd = findGCD(number1, number2);
+    printf("\nGCD of %i and %i = %i",number1, number2, gcd);
 
     return 0;
 }
@@ -23,4 +29,24 @@ int addNumbers(int n){
 
     return n + addNumbers(n - 1);
     
+}
+
+int findGCD(int number1, int number2){
+
+    while (number1 != number2)
+    {
+        if (number1 > number2)
+        {
+            return findGCD(number1 - number2, number2);
+        }
+        else
+        {
+            return findGCD(number1, number2 - number1);
+        }
+    
+    }
+
+    return number1;
+    
+
 }
